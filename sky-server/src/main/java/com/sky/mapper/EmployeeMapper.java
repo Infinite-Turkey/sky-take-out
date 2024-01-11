9f074589ac2f74af68af98dfd4b5c8ca0884018c
Jin <jinhu@buffalo.edu>
2024-01-10 17:08:56 -0800

    page helper

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 
 import org.apache.ibatis.annotations.Insert;
@@ -21,4 +23,11 @@ public interface EmployeeMapper {
             "values " +
     "(#{name}, #{username}, #{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
   void insert(Employee employee);
+
+  /**
+   * 员工分页查询
+   * @param employeePageQueryDTO
+   * @return
+   */
+  Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
