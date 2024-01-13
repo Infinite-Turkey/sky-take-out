7b727b7640d9514254296d29929334062a7fd7e8
Jin <jinhu@buffalo.edu>
2024-01-13 14:49:34 -0800

    dish pagehelper

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,10 +2,13 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,4 +39,14 @@ public class DishController {
     dishService.saveWithFlavor(dishDTO);
     return Result.success();
   }
+
+  @GetMapping("/page")
+  @ApiOperation("菜品分页查询")
+  //DishPageQueryDTO是query格式的，不是json格式，所以不需要@RequestBody
+  public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+    log.info("菜品分页查询：{}", dishPageQueryDTO);
+    PageResult pageResult= dishService.pageQuery(dishPageQueryDTO);
+    return Result.success(pageResult);
+  }
+
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
@@ -9,4 +11,11 @@ public interface DishService {
    * @param dishDTO
    */
   void saveWithFlavor(DishDTO dishDTO);
+
+  /**
+   * 菜品分页查询
+   * @param dishPageQueryDTO
+   * @return
+   */
+  PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
