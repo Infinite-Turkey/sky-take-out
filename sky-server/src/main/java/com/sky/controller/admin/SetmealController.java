84fd1f7fcae85896b94ec6130e5c8852474acc19
Jin <jinhu@buffalo.edu>
2024-01-21 03:48:58 -0800

    Revert "Cache meals"
    This reverts commit e597c1c28413bc3a5f77b120acac5de2b91f7a69.

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,7 +10,6 @@ import com.sky.vo.SetmealVO;
 import com.wechat.pay.contrib.apache.httpclient.util.RsaCryptoUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,7 +43,6 @@ public class SetmealController {
 
   @PostMapping
   @ApiOperation("新增套餐")
-  @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId")
   public Result save(@RequestBody SetmealDTO setmealDTO){
     setmealService.saveWithDish(setmealDTO);
     return Result.success();
@@ -69,7 +67,6 @@ public class SetmealController {
    */
   @DeleteMapping
   @ApiOperation("批量删除套餐")
-  @CacheEvict(cacheNames = "setmealCache", allEntries = true)
   public Result delete(@RequestParam List<Long> ids){
     setmealService.deleteBatch(ids);
     return Result.success();
@@ -94,7 +91,6 @@ public class SetmealController {
    */
   @PutMapping
   @ApiOperation("修改套餐")
-  @CacheEvict(cacheNames = "setmealCache", allEntries = true)
   public Result update(@RequestBody SetmealDTO setmealDTO){
     setmealService.update(setmealDTO);
     return Result.success();
@@ -108,7 +104,6 @@ public class SetmealController {
    */
   @PostMapping("status/{status}")
   @ApiOperation("套餐起售停售")
-  @CacheEvict(cacheNames = "setmealCache", allEntries = true)
   public Result<String> startOrStop(@PathVariable Integer status, Long id){
     setmealService.startOrStop(status, id);
     return Result.success();
